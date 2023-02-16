59ad96af94b8fe8e5f5a518a0bf17098b31a0d2a
hesham <he.shm.qeshmi@gmail.com>
2023-02-16 21:47:56 +0330

    Install Material UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { loader as rootLoader } from "./views/Root";
